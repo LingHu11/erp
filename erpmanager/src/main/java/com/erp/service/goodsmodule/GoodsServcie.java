d2530de37bd94e0544e663190ef1706da44f8866
Yuhq <7707392+yuhq_git@user.noreply.gitee.commkdir>
2020-08-12 17:53:51 +0800

    余浩琪测试er提交

--- erpmanager/src/main/java/com/erp/service/goodsmodule/GoodsServcie.java
+++ erpmanager/src/main/java/com/erp/service/goodsmodule/GoodsServcie.java
@@ -1,9 +1,13 @@
 package com.erp.service.goodsmodule;
 
+
+
 import com.erp.domain.goodsmodule.TbGoods;
 
 import java.util.List;
 
 public interface GoodsServcie {
     List<TbGoods> findAll();
+
+
 }
